b6f3c37e0d391a9f824d6a07009ff9ee950f7b2e
Alyssa Llanes <41835506+strangeanomalyy@users.noreply.github.com>
2024-05-20 06:18:13 +0000

    updated streamlit_app with new code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,9 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
 import streamlit as st
+import numpy as np
 import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
-df
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
